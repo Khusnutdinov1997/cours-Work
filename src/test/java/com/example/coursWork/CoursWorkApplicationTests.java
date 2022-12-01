74689ac6a82eb1b9032ca646c5049a0f0d7ac7da
Emil <emiles1997@gmail.com>
2022-12-01 09:01:25 +0300

    первый коммит

---
+++ src/test/java/com/example/coursWork/CoursWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
